69d6aeee60f962915a50b4e645d5341422d4b66b
mbieluszko-tech <m.bieluszko@gmail.com>
2026-03-19 08:30:56 +0100

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './src/App';
+import App from './orchestra-app';
 
 const root = document.getElementById('root');
 if (!root) throw new Error('Root element not found');
